29df01b4472b293c801dac0948919e18e629bb43
W. Matthew Wilson <matt@tplus1.com>
2010-01-28 22:23:52 -0500

    A little progress

--- listing1.py
+++ listing1.py
@@ -2,14 +2,17 @@
 
 import unittest
 
-class TestAsString(unittest.TestCase):
+class TestAsString1(unittest.TestCase):
+    """
+    This one is going to raise ERRORs.
+    """
 
     def test_1(self):
         """
         Convert an empty TaskDict to a string.
         """
 
-        td = Task()
+        td = self.Task()
         s = str(td)
 
         assert s == 'Empty task', \
@@ -22,7 +25,7 @@ class TestAsString(unittest.TestCase):
         Convert a Task with stuff inside it into a string.
         """
 
-        td = Task(title='Waterproof basement',
+        td = self.Task(title='Waterproof basement',
             expected_cost_range=[3000, 8000])
 
         s = str(td)
@@ -41,11 +44,54 @@ expected_cost_range            : [3000, 8000]
         "Got %s, expected something different!" % s
 
 
-# Uncomment the next lines to see the difference between errors and
-# failures.
-# class Task(object):
-#     def __init__(self, *args, **kwargs):
-#         pass
+class Task(dict):
+    """
+    Just like a dictionary, but with modified __str__ and __setitem__
+    methods.
+    """
+
+    def __str__(self):
+        if not self:
+            return "Empty task"
+
+        else:
+
+            return "\n".join(
+                [
+                    self['title'],
+                    "="*len(self['title']),
+                    "",
+                    "Attributes",
+                    "-"*len("Attributes"),
+                    ""]
+                + [
+                    "%-31s: %s" % (k, v)
+                    for k, v in self.items()
+                    if k != 'title']
+                + [""]
+                )
+
+
+class NullTask(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class TestAsString2(TestAsString1):
+
+    def setUp(self):
+        self.Task = NullTask
+
+
+class TestAsString3(TestAsString1):
+
+    """
+    This one should pass.
+    """
+
+    def setUp(self):
+        self.Task = Task
+
 
 
 if __name__ == '__main__':
